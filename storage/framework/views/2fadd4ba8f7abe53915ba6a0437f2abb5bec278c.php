510d2585468cd3ab95dd01a5bf61f1d352b20211
Avishek Barua <avishekbarua02@gmail.com>
2022-11-19 23:56:06 +0600

    before merge

--- storage/framework/views/2fadd4ba8f7abe53915ba6a0437f2abb5bec278c.php
+++ storage/framework/views/2fadd4ba8f7abe53915ba6a0437f2abb5bec278c.php
@@ -40,7 +40,65 @@
     <!-- Google Font -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-</head>
+
+
+<style>
+    body{
+    margin-top:20px;
+    color: #1a202c;
+    text-align: left;
+    background-color: #e2e8f0;    
+}
+.main-body {
+    padding: 15px;
+}
+.card {
+    box-shadow: 0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06);
+}
+
+.card {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 0 solid rgba(0,0,0,.125);
+    border-radius: .25rem;
+}
+
+.card-body {
+    flex: 1 1 auto;
+    min-height: 1px;
+    padding: 1rem;
+}
+
+.gutters-sm {
+    margin-right: -8px;
+    margin-left: -8px;
+}
+
+.gutters-sm>.col, .gutters-sm>[class*=col-] {
+    padding-right: 8px;
+    padding-left: 8px;
+}
+.mb-3, .my-3 {
+    margin-bottom: 1rem!important;
+}
+
+.bg-gray-300 {
+    background-color: #e2e8f0;
+}
+.h-100 {
+    height: 100%!important;
+}
+.shadow-none {
+    box-shadow: none!important;
+}
+</style>
+
+    </head>
 
 <body class="hold-transition skin-blue sidebar-mini <?php echo e(Request::is('pos*') ? 'sidebar-collapse ' : ''); ?>">
     <div class="wrapper">
@@ -74,8 +132,8 @@
                                         Admin
                                         <?php elseif((Auth::user()->is_admin) == 0): ?>
                                             Seller
-                                        <?php elseif((Auth::user()->is_admin) == 2): ?>
-                                            Purchaser
+                                        <?php elseif((Auth::user()->is_admin) == 5): ?>
+                                            Customer
                                         <?php endif; ?> )
                                     </p>
 
@@ -189,7 +247,7 @@
                     <?php endif; ?>
 
 
-                    <?php if((Auth::user()->is_admin) == 1 || (Auth::user()->is_admin) == 0 ): ?>
+                    <?php if((Auth::user()->is_admin) == 1 || (Auth::user()->is_admin) == 0|| (Auth::user()->is_admin) == 5 ): ?>
                     <li class="<?php echo e(Request::is('pos*') ? 'active' : ''); ?>">
                         <a href="<?php echo e(route('pos.index')); ?>">
                             <i class="fa fa-line-chart"></i> <span>Point Of Sale</span>
